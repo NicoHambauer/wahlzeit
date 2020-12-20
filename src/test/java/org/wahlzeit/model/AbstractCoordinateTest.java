ce6031b557c40ce9de3414d0ee61d553b02a97e0
NicoHambauer <nico.hambauer@fau.de>
2020-12-20 16:13:06 +0100

    adap-cw07 implementing AbstractCoordinateTest for CheckedCoordinateExceptions

--- src/test/java/org/wahlzeit/model/AbstractCoordinateTest.java
+++ src/test/java/org/wahlzeit/model/AbstractCoordinateTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Before;
 import org.wahlzeit.utils.CheckedCoordinateException;
+import org.wahlzeit.utils.UncheckedCoordinateException;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,28 +12,33 @@ import java.sql.SQLException;
 
 public class AbstractCoordinateTest extends AbstractCoordinate {
 
-    AbstractCoordinate abs_co1;
-    AbstractCoordinate abs_co2;
+    public AbstractCoordinate abs_co1;
+    public AbstractCoordinate abs_co2;
 
-    double x, y, z;
+    public static boolean testException;
 
-    double to_be_distance = 1.0;
-    double to_be_central_angle = Math.toRadians(45.0);
-    boolean to_be_equals = true;
+    public double x, y, z;
 
-    double maxdiff = 0.00001;
+    public double to_be_distance = 1.0;
+    public double to_be_central_angle = Math.toRadians(45.0);
+    public boolean to_be_equals = true;
+
+    public double maxdiff = 0.00001;
 
     public AbstractCoordinateTest(){
-        this.x = 0;
-        this.y = 0;
-        this.z = 0;
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
     }
+
 
     @Before //each
     public void init() {
-        abs_co1 = new AbstractCoordinateTest();
-        abs_co2 = new AbstractCoordinateTest();
+        this.testException = false;
+        this.abs_co1 = new AbstractCoordinateTest();
+        this.abs_co2 = new AbstractCoordinateTest();
     }
+
 
     @Test
     public void testCartesianDistance(){
@@ -45,6 +51,15 @@ public class AbstractCoordinateTest extends AbstractCoordinate {
         }
         //assert
         assertTrue(Math.abs(distance - to_be_distance) <= maxdiff);
+    }
+
+    @Test (expected = CheckedCoordinateException.class)
+    public void testCartesianDistanceException() throws CheckedCoordinateException {
+        //arrange see init act
+        double distance = 0.0;
+        testException = true;
+        distance = abs_co1.getCartesianDistance(abs_co2);
+
     }
 
     @Test
@@ -60,12 +75,28 @@ public class AbstractCoordinateTest extends AbstractCoordinate {
         assertTrue(Math.abs(central_angle - to_be_central_angle) <= maxdiff);
     }
 
+    @Test (expected = CheckedCoordinateException.class)
+    public void testCentralAngleException() throws CheckedCoordinateException{
+        //arrange see init act
+        testException = true;
+        double central_angle = 0.0;
+        central_angle = abs_co1.getCentralAngle(abs_co2);
+
+    }
+
     @Test
     public void testEqualsAndIsEqual(){
         //arrange see init and act
         boolean eq = abs_co1.equals(abs_co2);
         //assert
         assertTrue(eq);
+    }
+
+    @Test (expected = CheckedCoordinateException.class)
+    public void testIsEqualException() throws CheckedCoordinateException {
+        //arrange see init and act
+        testException = true;
+        boolean eq = abs_co1.isEqual(abs_co2);
     }
 
 
@@ -75,10 +106,17 @@ public class AbstractCoordinateTest extends AbstractCoordinate {
         return new CartesianCoordinate(){
             //stub with stub methods
             public double doGetDistance(CartesianCoordinate other_cartesian_Coordinate){
+                if(testException){
+                    throw new UncheckedCoordinateException("doGetDistance failed");
+                }
+
                 return to_be_distance;
             }
 
             public boolean doIsEqual(CartesianCoordinate other_cartesianCoordinate){
+                if(testException){
+                    throw new UncheckedCoordinateException("diIsEqual went wrong");
+                }
                 return to_be_equals;
             }
         };
@@ -89,6 +127,9 @@ public class AbstractCoordinateTest extends AbstractCoordinate {
         return new SphericCoordinate(){
             //stub with stub methods
             public double doGetCentralAngle(SphericCoordinate other_spheric_Coordinate){
+                if(testException){
+                    throw new UncheckedCoordinateException("doGetCentralAngle failed");
+                }
                 return to_be_central_angle;
             }
         };
